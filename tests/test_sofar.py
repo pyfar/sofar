6f89c92cad749a9de8408ab97658cf7f54f8614c
Fabian Brinkmann <fabian.brinkmann@mailbox.org>
2021-08-02 10:02:46 +0200

    add test for list_conventions

--- tests/test_sofar.py
+++ tests/test_sofar.py
@@ -1,5 +1,23 @@
 import sofar as sf
+import os
 from pytest import raises
+
+
+def test_list_conventions(capfd):
+
+    # check output to console using pytest default fixture capfd
+    sf.list_conventions()
+    out, _ = capfd.readouterr()
+    assert "Available SOFA conventions:" in out
+
+    sf.list_conventions(print_conventions=False)
+    out, _ = capfd.readouterr()
+    assert out == ""
+
+    # check the return type
+    paths = sf.list_conventions(print_conventions=False, return_paths=True)
+    assert isinstance(paths, list)
+    assert os.path.isfile(paths[0])
 
 
 def test_get_convention():
