6dad5cf3983607e0e78fe89c9cab1c56e545b8e1
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2022-11-03 09:44:37 +0100

    update urls for the project, code repo and docs

--- setup.py
+++ setup.py
@@ -57,7 +57,13 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/pyfar/sofar',
+    url="https://pyfar.org/",
+    download_url="https://pypi.org/project/pyfar/",
+    project_urls={
+        "Bug Tracker": "https://github.com/pyfar/sofar/issues",
+        "Documentation": "https://sofar.readthedocs.io/",
+        "Source Code": "https://github.com/pyfar/sofar",
+    },
     version='0.3.1',
     zip_safe=False,
     python_requires='>=3.8'
