7e425a37654c2bfeb66257bc65bdeccd6c65da75
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2024-03-15 17:06:14 +0100

    cleanup

--- docs/conf.py
+++ docs/conf.py
@@ -40,12 +40,10 @@ plot_include_source = True
 imgmath_latex_preamble = r'\usepackage{array}'
 
 # Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
 source_suffix = '.rst'
 
 # The master toctree document.
@@ -72,13 +70,9 @@ release = sofar.__version__
 # Usually you set "language" from the command line for these cases.
 language = 'en'
 
-
 # List of patterns, relative to source directory, that match files and
-templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-
-
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use (Not defining
@@ -112,7 +106,6 @@ html_logo = 'resources/sofar.png'
 html_title = "sofar"
 html_favicon = '_static/favicon.ico'
 
-
 # -- HTML theme options
 # https://pydata-sphinx-theme.readthedocs.io/en/stable/user_guide/layout.html
 
@@ -127,71 +120,13 @@ html_theme_options = {
           "icon": "fa-brands fa-square-github",
           "type": "fontawesome",
         },
-        {
-            "name": "CC-BY",
-            "url": "https://creativecommons.org/licenses/by/4.0/deed.de",
-            "icon": "fa-brands fa-creative-commons-by",
-            "type": "fontawesome",
-        }
     ],
-
-  # Configure secondary (right) side bar
-  "show_toc_level": 3,                     # Show all subsections of notebooks
-  "secondary_sidebar_items": ["page-toc"],  # Omit 'show source' link that that
-                                           # shows notebook in json format
-  "navigation_with_keys": True,
+    # Configure secondary (right) side bar
+    "show_toc_level": 3, # Show all subsections of notebooks
+    "secondary_sidebar_items": ["page-toc"],  # Omit 'show source' link that that shows notebook in json format
+    "navigation_with_keys": True,
 }
 
 html_context = {
-   "default_mode": "light"
+    "default_mode": "light"
 }
-# -- Options for Texinfo output ----------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-# texinfo_documents = [
-#     (master_doc, 'pyfar',
-#      u'pyfar Gallery',
-#      author,
-#      'pyfar',
-#      'One line description of project.',
-#      'Miscellaneous'),
-# ]
-
-# -- Options for nbsphinx -------------------------------------------------
-nbsphinx_prolog = r"""
-{% set docname = 'doc/' + env.doc2path(env.docname, base=None) %}
-
-.. raw:: html
-
-    <div class="admonition note">
-      Open an interactive online version by clicking the badge
-      <span style="white-space: nowrap;"><a href="https://mybinder.org/v2/gh/pyfar/gallery/main?filepath={{ docname|e }}"><img alt="Binder badge" src="https://mybinder.org/badge_logo.svg" style="vertical-align:text-bottom"></a></span>
-      or
-      <a href="{{ env.docname.split('/')|last|e + '.ipynb' }}" class="reference download internal" download>download</a>
-      the notebook.
-      <script>
-        if (document.location.host) {
-          let nbviewer_link = document.createElement('a');
-          nbviewer_link.setAttribute('href',
-            'https://nbviewer.org/url' +
-            (window.location.protocol == 'https:' ? 's/' : '/') +
-            window.location.host +
-            window.location.pathname.slice(0, -4) +
-            'ipynb');
-          nbviewer_link.innerHTML = 'Or view it on <em>nbviewer</em>';
-          nbviewer_link.classList.add('reference');
-          nbviewer_link.classList.add('external');
-          document.currentScript.replaceWith(nbviewer_link, '.');
-        }
-      </script>
-    </div>
-
-"""
-
-# -- manage thumbnails --------------------------------------------------------
-# must be located in 'docs/_static'
-nbsphinx_thumbnails = {
-}
-
