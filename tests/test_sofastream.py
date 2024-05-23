d27bc2a49637694add332f442d2d342b95505357
Anton Hoyer <a.hoyer@campus.tu-berlin.de>
2024-05-23 08:15:10 +0200

    test_sofastream: tempfile fixture, test for inspect() txt-output

--- tests/test_sofastream.py
+++ tests/test_sofastream.py
@@ -2,22 +2,26 @@ from sofar import SofaStream
 import sofar as sf
 from tempfile import TemporaryDirectory
 from pytest import raises
+import pytest
 import netCDF4
 import numpy as np
 import os
 
-tempdir = TemporaryDirectory()
-filename = os.path.join(tempdir.name, "test_sofastream.sofa")
-sofa = sf.Sofa("SimpleFreeFieldHRIR")
+# Temporary SOFA-file
+@pytest.fixture
+def temp_sofa_file(tmp_path_factory):
 
-sofa.Data_IR = np.array([[0, 1], [2, 3], [4, 5]])
-sofa.GLOBAL_RoomType = "free field"
-sofa.Data_SamplingRate_Units = "hertz"
-sf.write_sofa(filename, sofa)
+    filename = tmp_path_factory.mktemp("data") / "test_sofastream.sofa"
+    sofa = sf.Sofa("SimpleFreeFieldHRIR")
+    sofa.Data_IR = np.array([[0, 1], [2, 3], [4, 5]])
+    sofa.GLOBAL_RoomType = "free field"
+    sofa.Data_SamplingRate_Units = "hertz"
+    sf.write_sofa(filename, sofa)
+    return filename
 
-def test_sofastream_output():
+def test_sofastream_output(temp_sofa_file):
 
-    with SofaStream(filename) as file:
+    with SofaStream(temp_sofa_file) as file:
         obj = file
         var = file.Data_IR
         var_data = var[:]
@@ -30,21 +34,26 @@ def test_sofastream_output():
         isinstance(var, netCDF4._netCDF4.Variable)
         # variable values
         isinstance(var_data, np.ma.core.MaskedArray)
-        np.testing.assert_array_equal(var_data.squeeze(), sofa.Data_IR)
+        np.testing.assert_array_equal(var_data.squeeze(),
+                                      np.array([[0, 1], [2, 3], [4, 5]]))
         assert var_attr == 'hertz'
         # attribute values
         isinstance(att_data, str)
-        assert att_data == sofa.GLOBAL_RoomType
+        assert att_data == "free field"
 
-def test_sofastream_warnings():
+def test_sofastream_warnings(temp_sofa_file):
 
-    with SofaStream(filename) as file:
+    with SofaStream(temp_sofa_file) as file:
         with raises(ValueError, match="attribute is not in dataset"):
             file.Wrong_Attribute
 
-def test_sofastream_inspect(capfd):
-    with SofaStream(filename) as file:
-        file.inspect()
+def test_sofastream_inspect(capfd, temp_sofa_file):
+
+    tempdir = TemporaryDirectory()
+    inspect_file = os.path.join(tempdir.name, "info.txt")
+
+    with SofaStream(temp_sofa_file) as file:
+        file.inspect(inspect_file)
         out, _ = capfd.readouterr()
         assert "GLOBAL_SOFAConventions : SimpleFreeFieldHRIR" in out
         assert "Data_SamplingRate_Units : hertz" in out
@@ -52,3 +61,8 @@ def test_sofastream_inspect(capfd):
                 "  [[0. 1.]\n"
                 "   [2. 3.]\n"
                 "   [4. 5.]]") in out
+
+    # check text file
+    with open(inspect_file, "r") as out_inspect:
+        text = out_inspect.readlines()
+    assert out == "".join(text)
